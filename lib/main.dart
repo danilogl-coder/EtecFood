07e09b079d43d6f95058756ff726cf1371c7a91a
Danilo Silva <daniloglcomercial@gmail.com>
2023-08-08 22:59:43 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:etecfood/models/app.dart';  //<- Importei app(MaterialApp)
 import 'package:etecfood/models/page_manager.dart'; //<- importei o page_manager(gerenciador de paginas.)
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart'; //<- importei material dart.
-import 'package:flutter_modular/flutter_modular.dart'; //<- importei a biblioteca flutter_modular.
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'firebase_options.dart'; //<- importei a biblioteca flutter_modular.
 
 void main() async {
 // Esse comando esta garantindo inicialização correta antes do flutter iniciar
