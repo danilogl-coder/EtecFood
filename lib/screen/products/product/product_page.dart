1afb7e1bab2eb7136a3d2c19d04ef0c4aeadc52f
Danilo <daniloglcomercial@gmail.com>
2023-10-03 20:17:34 -0300

    .

--- lib/screen/products/product/product_page.dart
+++ lib/screen/products/product/product_page.dart
@@ -11,68 +11,85 @@ class ProductPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(product.name!),
-        centerTitle: true,
-      ),
-      body: ListView(
-        children: [
-          FlutterCarousel(
-            options: CarouselOptions(
-              height: 270.0,
-              showIndicator: true,
-              slideIndicator: const CircularSlideIndicator(),
-            ),
-            items: product.images!.map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      decoration: const BoxDecoration(color: Colors.white),
-                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Image.network(i));
-                },
-              );
-            }).toList(),
+      body: CustomScrollView(
+
+        slivers: [
+          SliverAppBar(
+            title: Text(product.name!),
           ),
-         Padding(padding: const EdgeInsets.symmetric(horizontal: 20.0),
-         child: Column(
-          children: [
-            Text(product.name!, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
-            const Divider(thickness: 2, color: Colors.black,),
-             const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          SliverToBoxAdapter(
+            child: FlutterCarousel(
+              options: CarouselOptions(
+                height: 270.0,
+                showIndicator: true,
+                slideIndicator: const CircularSlideIndicator(),
+              ),
+              items: product.images!.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(color: Colors.white),
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Image.network(i));
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+
+         SliverToBoxAdapter(
+           child: Padding(padding: const EdgeInsets.symmetric(horizontal: 20.0),
+           child: Column(
+            children: [
+              Text(product.name!, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
+              const Divider(thickness: 2, color: Colors.black,),
+               const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                 Text('Por R\$:' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0,),),
+                   Text('19.99' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.green),)
+              ],),
+              const Row(
+                children: [
+                  Text('Descrição:' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 40,
+                    child: Text('${product.description!} ddddddd ddddddddd dddddddd ddddddd  dddddddddddddddddd dddddddddddddddd dddddddddddd ddddddddddd dddddddddd ddddddddddddd ddddddddddddddd ddddddddddd ddddddddddd' ,style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14.0), softWrap: true, maxLines: 20,)),
+                ],
+              ),
+              
+                
+            ],
+           ),),
+           
+         ),
+      
+          SliverFillRemaining(
+            
+            child: Column(
               children: [
-               Text('Por R\$:' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0,),),
-                 Text('19.99' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.green),)
-            ],),
-            const Row(
-              children: [
-                Text('Descrição:' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),)
+                Expanded(child: Container()),
+                SizedBox(
+                          height: 44,
+                          child: ElevatedButton(
+                            onPressed:() {},
+                           style: ElevatedButton.styleFrom(
+                                backgroundColor: Color.fromARGB(255, 65, 69, 110)),
+                            child: const Text('Adicionar ao carrinho', style: TextStyle(
+                                  color: Colors.white, fontSize: 18.0),
+                            ),
+                          ),
+                        ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(product.description! ,style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14.0), softWrap: true,),
-              ],
-            ),
-            const SizedBox(
-              height: 150,
-            ),
-             SizedBox(
-                    height: 44,
-                    child: ElevatedButton(
-                      onPressed:() {},
-                     style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 65, 69, 110)),
-                      child: const Text('Adicionar ao carrinho', style: TextStyle(
-                            color: Colors.white, fontSize: 18.0),
-                      ),
-                    ),
-                  )
-          ],
-         ),)
+          ),
+          
         ],
       ),
     );
