37ba82c29e06c5e40222996cacce1d21767c6120
Danilo <daniloglcomercial@gmail.com>
2023-11-27 21:29:52 -0300

    .

--- lib/screen/base/base_module.dart
+++ lib/screen/base/base_module.dart
@@ -6,12 +6,20 @@ import 'package:etecfood/models/auth_guard.dart';
 import 'package:etecfood/screen/base/base_controller.dart';
 import 'package:etecfood/screen/base/base_cubit.dart';
 import 'package:etecfood/screen/base/base_screen.dart';
+import 'package:etecfood/screen/products/products_controller.dart';
+import 'package:etecfood/screen/products/products_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class BaseModule extends Module {
   @override
   void binds(i) {
+  i.addSingleton<FirebaseCartHelper>(FirebaseCartHelper.new);
+  i.addLazySingleton<FirebaseProductHelper>(FirebaseProductHelper.new);
+  i.addLazySingleton<ProductCubit>(ProductCubit.new);
+  i.addInstance<ProductController>(
+  ProductController(helper: i(), productCubit: i(), cartHelper: i()));
+
   i.addLazySingleton<FirebaseBaseHelper>(FirebaseBaseHelper.new);
   i.addLazySingleton<BaseCubit>(BaseCubit.new);
   i.addInstance<BaseController>(BaseController(helper: i(), baseCubit: i()));
